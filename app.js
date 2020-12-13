570f2c91bd9bafa5df975eed9b0b1b001a3abc9f
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-12-13 22:18:56 +0530

    Removed users route

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
+// Setting up express app
 const app = express();
 
 // view engine setup
@@ -37,8 +37,8 @@ app.use(compression());
 // For Cookie parsing
 app.use(cookieParser());
 
+// Routes handler
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
