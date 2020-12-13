fe43e6e7003649ac118082d7642fbd5d016f48e3
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-12-13 22:24:15 +0530

    Added dummy routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET About page. */
+router.get('/about', (req, res, next) => {
+  res.render('index');
+});
+
+/* GET Contact us page. */
+router.get('/contactus', (req, res, next) => {
+  res.render('index');
 });
 
 module.exports = router;
