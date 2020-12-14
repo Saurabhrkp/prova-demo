ccf64e7d45b176e39230c447531f3d1a137221e2
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-12-14 21:56:22 +0530

    Added data for navbar active check

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,27 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', { current: 'home' });
+});
+
+/* GET Products page. */
+router.get('/products', (req, res, next) => {
+  res.render('products', { current: 'products' });
+});
+
+/* GET Product page. */
+router.get('/product', (req, res, next) => {
+  res.render('product', { current: 'products' });
 });
 
 /* GET About page. */
-router.get('/about', (req, res, next) => {
-  res.render('aboutus');
+router.get('/aboutus', (req, res, next) => {
+  res.render('aboutus', { current: 'aboutus' });
 });
 
 /* GET Contact us page. */
 router.get('/contactus', (req, res, next) => {
-  res.render('contactus');
+  res.render('contactus', { current: 'contactus' });
 });
 
 module.exports = router;
